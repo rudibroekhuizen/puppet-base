1b075a85d05c2c0f29a2671c84bc9416f7fac70f
rudibroekhuizen <rudi.broekhuizen@naturalis.nl>
2014-08-25 17:47:51 +0200

    Create config.pp

---
+++ manifests/config.pp
@@ -0,0 +1,10 @@
+# == Class: base::config
+#
+class base::config {
+
+  host { $::fqdn:
+    ensure       => 'present',
+    host_aliases => [$::hostname, 'localhost'],
+    ip           => $::ipaddress,
+    target       => '/etc/hosts',
+  }
