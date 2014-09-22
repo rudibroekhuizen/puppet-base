e8e16edc6e023aa2444e9d5d95b15225631331b5
rudibroekhuizen <rudi.broekhuizen@naturalis.nl>
2014-09-22 23:31:14 +0200

    Update define_users.pp

--- manifests/define_users.pp
+++ manifests/define_users.pp
@@ -21,6 +21,20 @@ define base::define_users (
 
   group { $username: 
     ensure => present, 
-  }  
+  }
+
+  file { "/home/${username}": 
+    ensure  => directory, 
+    owner   => $username, 
+    group   => $username, 
+    mode    => '0700', 
+  } 
+
+  wget::fetch { "${username} .screenrc":
+    source      => "http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic",
+    destination => "/home/${username}/.screenrc",
+    timeout     => 0,
+    verbose     => false,
+  }
 
 }
