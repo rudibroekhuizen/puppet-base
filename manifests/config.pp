f3673bbb6c3470490b0568b4181df856226b5bd6
rudibroekhuizen <rudi.broekhuizen@naturalis.nl>
2014-09-22 17:06:48 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,4 +9,12 @@ class base::config {
     ip           => $::ipaddress,
     target       => '/etc/hosts',
   }
+  
+  wget::fetch { "grml-etc-core .screenrc":
+    source      => 'http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic',
+    destination => '/etc/screenrc/.screenrc',
+    timeout     => 0,
+    verbose     => false,
+  }
+  
 }
