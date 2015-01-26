33a0f0459bfc950a61cd0f4c77bb5867fbe0062f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-26 10:54:25 +0100

    Update files.pp

--- manifests/files.pp
+++ manifests/files.pp
@@ -7,21 +7,10 @@ class base::files {
     source  => 'puppet:///modules/role_base/Puppetfile',
   }
 
-  # Hiera config file
-  #file { '/etc/puppet/hiera.yaml':
-  #  source  => 'puppet:///modules/base/hiera.yaml',
-  #}
-  
-  # Create hieradata directory
-  #file { '/etc/puppet/hieradata':
-  #  ensure => 'directory',
-  #}
-
   # Hiera user data
-  file { '/etc/puppet/hieradata/global.yaml':
-    source  => 'puppet:///modules/role_base/global.yaml',
-    #replace => false,
-    #require => File['/etc/puppet/hieradata'],
+  file { '/etc/puppet/hieradata':
+    source  => 'puppet:///modules/role_base',
+    recurse => true
   }
 
 }
