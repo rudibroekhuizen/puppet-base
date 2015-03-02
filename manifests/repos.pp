266806b74590584ab619bb8b01c1b73803a7f04f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-03-02 16:33:17 +0100

    Update repos.pp

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -5,7 +5,7 @@ class base::repos {
   case $::osfamily {
     debian: { 
       include apt
-      apt::ppa { $::repos_array_debian: }
+      apt::ppa { $base::repos_array_debian: }
     }
     redhat: {
       include epel
