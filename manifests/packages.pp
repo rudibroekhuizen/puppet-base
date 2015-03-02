5d57cbf765576aed9a3f006fd4d81d8a464b81e6
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-03-02 15:49:53 +0100

    Update packages.pp

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,13 +1,9 @@
 # == Class: base::packages
 #
-class base::packages (
-  $packages_hash_debian,
-  $packages_hash_redhat,
-  $packages_attribute_defaults,
-  ){
+class base::packages {
 
 case $::osfamily {
-  debian: { create_resources('base::defines::packages', $packages_hash_debian, $packages_attribute_defaults) }
-  redhat: { create_resources('base::defines::packages', $packages_hash_redhat, $packages_attribute_defaults) }
+  debian: { create_resources('base::defines::packages', $::packages_hash_debian, $::packages_attribute_defaults) }
+  redhat: { create_resources('base::defines::packages', $::packages_hash_redhat, $::packages_attribute_defaults) }
   }
 }
