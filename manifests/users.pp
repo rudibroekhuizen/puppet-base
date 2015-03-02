47db874297820167ea01f6851cb5aade8ed234c6
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-03-02 15:50:47 +0100

    Update users.pp

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,9 +1,6 @@
 # == Class: base::users
 #
-class base::users (
-  $users_hash,
-  $users_attribute_defaults,
-  ){
+class base::users {
 
-  create_resources('base::defines::users', $users_hash, $users_attribute_defaults)
+  create_resources('base::defines::users', $::users_hash, $::users_attribute_defaults)
 }
