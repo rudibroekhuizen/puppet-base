9490ea747fba6623ce3f1bd2d3c29b27f42a2eba
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-20 09:20:04 +0200

    Defaults to data folder

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,44 +36,14 @@
 # Copyright 2014 Your name here, unless otherwise noted.
 #
 class base (
-  $repos_array_debian = [ 'ppa:webupd8team/atom',
-                          #'ppa:webupd8team/java',
-                        ],
-
+  $repos_array_debian = undef,
   $vcsrepo_hash = undef,
-
-  $owner_email = 'rudi.broekhuizen@naturalis.nl',
-                
-  $packages_attribute_defaults = { ensure => installed,
-                                 },
-  $packages_hash_debian,
-  #$packages_hash_debian = { 'puppet' => { ensure => latest,
-  #                                      },
-
-  #                          'htop'   => { ensure => latest,
-  #                                      },
-  #                        },
-                         
-  $packages_hash_redhat = { 'puppet' => { ensure => latest,
-                                        },
-
-                            'htop'   => { ensure => latest,
-                                        },
-                          },
-                         
-  $users_attribute_defaults = { ensure => present,
-                              },
-                             
-  $users_hash = { 'rudi.broekhuizen' => { comment => 'Rudi Broekhuizen',
-                                          email   => 'rudi.broekhuizen@naturalis.nl',
-                                          groups  => ['wheel','sudo','adm','plugdev'],
-                                          ensure  => present,
-                                          shell   => '/bin/zsh',
-                                          ssh_key => { type => "ssh-rsa",
-                                                       key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDeRrVuojExYBWgIV7UlLfYLvzGpW9hSzrjl9qJ7Kb3E/x+kq2ruma3LXlvMzFXoCKJMYvqvXUFj2Dzwh+iJ2bCyh5ilWgTMLvAwzRl1LdAV8IbDVYNZGCHCzXkyxLMAEdxXMHVYl6N/q+RBP+HQCySpwOjv86c5PgXoL4BOnxLy5TAoxSsfvxRcHT42ThYK1C/R5QC8UoBdpJ1RBhQRjBJPMWp9zRyOafNwE7Iti15gKHp04bn9UGqHRTA1ul+Q6YEFzjoqUxe4VCHObM6BS/DpD++TPMXjzU+aeJ8tkxhmYkGRKYJx/KK3uaKPv+/EkODePvwTVshiQ8v9qOXx0YJ",
-                                                     },
-                                        },
-               },
+  $owner_email = undef,
+  $packages_attribute_defaults = undef,
+  $packages_hash_debian = undef,
+  $packages_hash_redhat = undef,
+  $users_attribute_defaults = udef,
+  $users_hash = undef,
   ) {
 
 # Various configure items
