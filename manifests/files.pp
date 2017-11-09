75c094c5c7aa9722f03fd7ba6472a689f1a63687
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-09 09:52:05 +0100

    Update files.pp

--- manifests/files.pp
+++ manifests/files.pp
@@ -3,12 +3,12 @@
 class base::files {
 
   # Puppetfile
-  file { '/etc/puppet/Puppetfile':
+  file { '/etc/puppetlabs/puppet/Puppetfile':
     source  => 'puppet:///modules/role_base/Puppetfile',
   }
 
   # Hiera user data
-  file { '/etc/puppet/data':
+  file { '/etc/puppetlabs/puppet/data':
     source  => 'puppet:///modules/role_base',
     recurse => true
   }
